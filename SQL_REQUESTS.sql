763a72439904af4acfa73c9436911d75759211ae
Kurkov1990 <kurkovandrey90@gmail.com>
2025-01-23 21:12:34 +0200

    Add file with SQL requests

---
+++ SQL_REQUESTS.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE my_database;
+
+CREATE TABLE IF NOT EXISTS users (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       age INT NOT NULL,
+                       email VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (name, age, email)
+VALUES
+    ('John', 30, 'john@example.com'),
+    ('Alice', 25, 'alice@example.com'),
+    ('Bob', 35, 'bob@example.com');
+
+SELECT id, name, age, email FROM users
+ORDER BY name;
+
+DELETE FROM users WHERE name = 'Bob';
+
+SELECT id, name, age, email FROM users
+ORDER BY age;
+
+SELECT id, name, age, email FROM users WHERE name = 'Bob';
