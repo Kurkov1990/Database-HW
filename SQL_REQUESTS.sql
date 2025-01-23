a36a52becf1ff60c817d1689923de4cc149b0cd4
Kurkov1990 <kurkovandrey90@gmail.com>
2025-01-23 21:28:18 +0200

    Add comments to SQL requests

--- SQL_REQUESTS.sql
+++ SQL_REQUESTS.sql
@@ -1,5 +1,7 @@
+-- Створення бази даних --
 CREATE DATABASE my_database;
 
+-- Створення таблиці даних --
 CREATE TABLE IF NOT EXISTS users (
                        id SERIAL PRIMARY KEY,
                        name VARCHAR(255) NOT NULL,
@@ -7,18 +9,21 @@ CREATE TABLE IF NOT EXISTS users (
                        email VARCHAR(255) NOT NULL
 );
 
+-- Вставка даних у таблицю --
 INSERT INTO users (name, age, email)
 VALUES
     ('John', 30, 'john@example.com'),
     ('Alice', 25, 'alice@example.com'),
     ('Bob', 35, 'bob@example.com');
 
+-- Перевірка, виведення таблиці--
 SELECT id, name, age, email FROM users
 ORDER BY name;
 
+-- Видалення користувача з вказаним ім'ям, виведення таблиці
 DELETE FROM users WHERE name = 'Bob';
-
 SELECT * FROM users
 ORDER BY age;
 
+-- Перевірка видалення, виведення інформації з фільтром по імені корисьтувача
 SELECT id, name, age, email FROM users WHERE name = 'Bob';
